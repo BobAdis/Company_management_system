423ea0092a1aca5c009f38ba7f6be10293c1fff3
BobAdis <romhanyi.botond@gmail.com>
2022-09-05 20:08:19 +0200

    Original

---
+++ src/main/java/hu/progmatic/company_management_system/CompanyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package hu.progmatic.company_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/progmatic/company_management_system/CompanyManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmatic.company_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
