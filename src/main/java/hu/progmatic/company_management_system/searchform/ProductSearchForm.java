e5400b4e9c42d22eb62b9821915dab67a8f145d3
GyurcsikR <gyurcsikr@gmail.com>
2022-09-10 13:24:59 +0200

    searchform

---
+++ src/main/java/hu/progmatic/company_management_system/searchform/ProductSearchForm.java
@@ -0,0 +1,24 @@
+package hu.progmatic.company_management_system.searchform;
+
+public class ProductSearchForm {
+
+    private String name;
+
+    private int serialNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+}
