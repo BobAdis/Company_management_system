095b47cd4776a48c26e94d8c6f76cb63b407d52d
BobAdis <romhanyi.botond@gmail.com>
2022-10-01 13:07:24 +0200

    RB: warehouse

--- src/main/java/hu/progmatic/company_management_system/repositories/RawMaterialRepo.java
+++ src/main/java/hu/progmatic/company_management_system/repositories/RawMaterialRepo.java
@@ -2,14 +2,14 @@ package hu.progmatic.company_management_system.repositories;
 
 
 import hu.progmatic.company_management_system.models.RawMaterial;
+import hu.progmatic.company_management_system.models.Warehouse;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+
 public interface RawMaterialRepo extends CrudRepository<RawMaterial, Long> {
 
     List<RawMaterial> findAll();
-
-    List<RawMaterial> getRawMaterialByWarehouse(String warehouse);
-
+    List<RawMaterial> getRawMaterialByWarehouse(Warehouse warehouse);
 }
