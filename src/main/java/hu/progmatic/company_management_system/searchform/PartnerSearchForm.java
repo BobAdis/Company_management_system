cac9ff204f8e2309d4b92ed015453ff526d9ef5a
BobAdis <romhanyi.botond@gmail.com>
2022-10-05 17:42:34 +0200

    RB: Partner modifications

---
+++ src/main/java/hu/progmatic/company_management_system/searchform/PartnerSearchForm.java
@@ -0,0 +1,14 @@
+package hu.progmatic.company_management_system.searchform;
+
+public class PartnerSearchForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
